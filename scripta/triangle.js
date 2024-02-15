d7e524a561fa170555cf44ed21b96509e0b67ae4
Sabbir Hossain <sanmoon1971@gmail.com>
2024-02-15 08:28:01 +0600

    Second commit

---
+++ scripta/triangle.js
@@ -0,0 +1,14 @@
+
+function calculateTriangleArea(){
+  const triangleBaseInput = document.getElementById('triangle-base');
+  const triangleBaseText = triangleBaseInput.value;
+  const base = parseFloat(triangleBaseText);
+  console.log(base);
+
+  const triangleHeightInput = document.getElementById('triangle-height');
+  const triangleHeightText = triangleHeightInput.value;
+  const height = parseFloat(triangleHeightText);
+  console.log(height);
+}
+
+
